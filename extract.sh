8618d8d451c69b2210885cd88564437b2377a974
khashab2 <khashab2@illinois.edu>
2019-07-01 16:34:18 -0400

    add script

---
+++ extract.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+filename="perspectrum_claims.txt"
+while read -r line; do
+    name="$line"
+    echo "Name read from file - $name"
+    python3.7 gquestions.py query "$name" en --csv
+    sleep 20
+done < "$filename"
+
+#python3.7 gquestions.py query "School Day Should Be Extended" en --csv
