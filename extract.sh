b644a138d75456ff57471589a77b853f7e3ac608
khashab2 <khashab2@illinois.edu>
2019-07-03 14:22:15 -0400

    everything looks done

--- extract.sh
+++ extract.sh
@@ -4,7 +4,7 @@ filename="perspectrum_claims.txt"
 while read -r line; do
     name="$line"
     echo "Name read from file - $name"
-    python3.7 gquestions.py query "$name" en --csv
+    python3.7 gquestions.py query "$name" en --csv  --headless 
     sleep 20
 done < "$filename"
 
